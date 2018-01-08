8d4aba92d2cc66df2ed9dd1fe1506d57c4c8f754
NikolayNN <kolya.by@mail.ru>
2018-01-08 23:12:05 +0300

    async validation for registration form - existed email or login

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {User} from "../../shared/models/user.model";
-import {UsersService} from "../../shared/services/users.service";
-import {Router} from "@angular/router";
+import {User} from '../../shared/models/user.model';
+import {UsersService} from '../../shared/services/users.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -19,11 +19,12 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'email': new FormControl(null, [Validators.required, Validators.email], this.checkForEmail.bind(this)),
       'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
-      'login': new FormControl(null, [Validators.required]),
+      'login': new FormControl(null, [Validators.required], this.checkForLogin.bind(this)),
       'agree': new FormControl(false, [Validators.requiredTrue])
     });
+    console.log(this.form);
   }
 
   onSubmit() {
@@ -35,7 +36,34 @@ export class RegistrationComponent implements OnInit {
           queryParams: {
             nowCanLogin: true
           }
-        })
+        });
       });
   }
+
+  checkForLogin(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserByLogin(control.value)
+        .subscribe((user: User) => {
+          if (!(user === undefined) && user.id > 0) {
+            resolve({forbiddenLogin: true});
+          } else {
+            resolve(null);
+          }
+        });
+    });
+  }
+
+  checkForEmail(control: FormControl): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.userService.getUserByEmail(control.value)
+        .subscribe((user: User) => {
+          if (!(user === undefined) && user.id > 0) {
+            resolve({forbiddenEmail: true});
+          } else {
+            resolve(null);
+          }
+        });
+    });
+  }
+
 }
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -15,6 +15,12 @@ export class UsersService {
       .map((user: User) => user.id > 0 ? user : undefined);
   }
 
+  getUserByEmail(email: string): Observable<User> {
+    return this.http.get(`http://localhost:8080/user?email=${email}`)
+      .map((response: Response) => response.json())
+      .map((user: User) => user.id > 0 ? user : undefined);
+  }
+
   createNewUser(user: User): Observable<User> {
     return this.http.post('http://localhost:8080/user', user)
       .map((response: Response) => response.json());
