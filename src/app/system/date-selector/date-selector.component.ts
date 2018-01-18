4e51d8b65efffe49ab495129a637ed889cf2ce8f
Nikolay <Horushko.Nikolay@gmail.com>
2018-01-18 22:57:49 +0300

    save date and time in date-selector.component

--- src/app/system/date-selector/date-selector.component.ts
+++ src/app/system/date-selector/date-selector.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
-declare var jquery: any;
-declare var $: any;
+declare var jquery:any;
+declare var $:any;
 
 @Component({
   selector: 'app-date-selector',
@@ -10,31 +10,73 @@ declare var $: any;
 })
 export class DateSelectorComponent implements OnInit {
 
-  startDate = '0000';
+  startDate = '';
+  startTime = '';
+
+  finishDate = '';
+  finishTime = '';
 
   constructor() {
   }
 
   ngOnInit() {
+    const today = this.getCurrentDate();
+    this.startDate = today;
+    this.startTime = '00:00';
+    this.finishDate = today;
+    this.finishTime = '23:59';
   }
 
-  showCalendar() {
-    $('.datepicker').pickadate({
+  getCurrentDate() {
+    const today = new Date();
+    let dd = today.getDate().toString();
+    let mm = (today.getMonth() + 1).toString();
+    const yyyy = today.getFullYear().toString();
+
+    if (dd.length === 1) {
+      dd = '0' + dd;
+    }
+
+    if (mm.length === 1) {
+      mm = '0' + mm;
+    }
+    return yyyy + '-' + mm + '-' + dd;
+  }
+
+  showCalendarStart() {
+    $('#startDate').pickadate({
       selectMonths: true, // Creates a dropdown to control month
       selectYears: 15, // Creates a dropdown of 15 years to control year,
       today: 'Today',
-      clear: 'Clear',
+      clear: false,
+      format: 'yyyy-mm-dd',
       close: 'Ok',
-      closeOnSelect: false // Close upon selecting a date,
+      closeOnSelect: true
     })
       .on('change', () => {
-        this.startDate = $('.datepicker').val();
-        console.log('triggered change');
+          this.startDate = $('#startDate').val();
+          console.log('startDate' + this.startDate);
       });
   }
 
-  showClock() {
-    $('.timepicker').pickatime({
+  showCalendarFinish() {
+    $('#finishDate').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      close: 'Ok',
+      clear: false,
+      format: 'yyyy-mm-dd',
+      closeOnSelect: true
+    })
+      .on('change', () => {
+        this.finishDate = $('#finishDate').val();
+        console.log('finishDate' + this.finishDate);
+      });
+  }
+
+  showClockStart() {
+    $('#startTime').pickatime({
       default: 'now', // Set default time: 'now', '1:30AM', '16:30'
       fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
       twelvehour: false, // Use AM/PM or 24-hour format
@@ -43,8 +85,25 @@ export class DateSelectorComponent implements OnInit {
       canceltext: 'Cancel', // Text for cancel-button
       autoclose: false, // automatic close timepicker
       ampmclickable: true, // make AM PM clickable
-      aftershow: function () {
-      } // Function for after opening timepicker
+    }).on('change', () => {
+      this.startTime = $('#startTime').val();
+      console.log('startTime' + this.startTime);
+    });
+  }
+
+  showClockFinish() {
+    $('#finishTime').pickatime({
+      default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+      fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+      twelvehour: false, // Use AM/PM or 24-hour format
+      donetext: 'OK', // text for done-button
+      cleartext: 'Clear', // text for clear-button
+      canceltext: 'Cancel', // Text for cancel-button
+      autoclose: false, // automatic close timepicker
+      ampmclickable: true, // make AM PM clickable
+    }).on('change', () => {
+      this.finishTime = $('#finishTime').val();
+      console.log('finishTime' + this.finishTime);
     });
   }
 }
